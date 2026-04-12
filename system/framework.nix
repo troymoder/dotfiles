ae22cbbe44503b88707df73e5b367fe8f992c4e1
Troy Benson <me@troymoder.dev>
2026-04-12 19:48:06 +0200

    try plymouth

--- system/framework.nix
+++ system/framework.nix
@@ -1,4 +1,31 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  gdmPlymouthTheme = pkgs.stdenv.mkDerivation {
+    pname = "gdm-plymouth-theme";
+    version = "1.0";
+    dontUnpack = true;
+
+    installPhase = ''
+      mkdir -p $out/share/plymouth/themes/gdm
+      cp -r ${pkgs.plymouth}/share/plymouth/themes/spinner/* \
+            $out/share/plymouth/themes/gdm/
+
+      # Replace background with GNOME default wallpaper
+      cp ${pkgs.gnome-backgrounds}/share/backgrounds/gnome/adwaita-d.jxl \
+         $out/share/plymouth/themes/gdm/background.png || \
+      cp ${pkgs.gnome-backgrounds}/share/backgrounds/gnome/adwaita-d.png \
+         $out/share/plymouth/themes/gdm/background.png
+
+      # Rename descriptor
+      mv $out/share/plymouth/themes/gdm/spinner.plymouth \
+         $out/share/plymouth/themes/gdm/gdm.plymouth
+
+      find $out/share/plymouth/themes/ -name \*.plymouth \
+        -exec sed -i "s@/usr/@$out/@" {} \;
+      find $out/share/plymouth/themes/ -name \*.plymouth \
+        -exec sed -i "s@Name=Spinner@Name=GDM@" {} \;
+    '';
+  };
+in {
   modules = {
     audio.enable = true;
     bluetooth.enable = true;
@@ -42,7 +69,8 @@
 
   boot.plymouth = {
     enable = true;
-    theme = "breeze";
+    theme = "gdm";
+    themePackages = [ gdmPlymouthTheme ]
   };
 
   boot.kernelParams = [ "quiet" "splash" "loglevel=3" "rd.udev.log_level=3" ];
